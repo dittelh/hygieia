314c7f16309f29be65cfb41dc791ede59f52006c
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-05-18 21:46:02 +0200

    slideshow and small changes

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ function openMenu() {
     }
   }
 }
+
+
+
+// Funktion til at skifte mellem kundeudtalelser
+var activeId = 0; 
+
+function nextClient(){
+  if(activeId !== 2){
+    activeId = activeId + 1;
+  } else {
+    activeId = 0;
+  }
+
+  for (let i = 0; i < 3; i++){
+    if(i == activeId){
+      document.getElementById('client' + i).classList.add('clientActive');
+    } else{
+      document.getElementById('client' + i).classList.remove('clientActive')
+    }
+  }
+}
+
+function previousClient(){
+  if(activeId !== 0){
+    activeId = activeId - 1;
+  } else {
+    activeId = 2;
+  }
+
+  for (let i = 0; i < 3; i++){
+    if(i == activeId){
+      document.getElementById('client' + i).classList.add('clientActive');
+    } else{
+      document.getElementById('client' + i).classList.remove('clientActive')
+    }
+  }
+}
