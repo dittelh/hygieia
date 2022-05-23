ca9155d5ca1e71945ebd109112f7450c3cbfba01
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-05-23 17:08:47 +0200

    faq js done

--- faq/faq.js
+++ faq/faq.js
@@ -1,11 +1,14 @@
 
-function openP() {
-    var answer = document.getElementById("hideP")
-    answer.classlist.toggle("showP")
-}
+function openAnswer(id){
 
-function openAnswer(){
-    var answer = document.getElementsByClassName("answer" + id)
-    answer[0].classList.toggle("answerSection")
-}
+    var answer = document.getElementById("answer" + id);
+    answer.classList.toggle("hideAnswer");
+    answer.classList.toggle("answerSection");
 
+    var img = document.getElementById("img" + id);
+    if(answer.classList.contains("answerSection")){
+        img.src = "../assets/img/minus.svg";
+    }else {
+        img.src = "../assets/img/plus.svg";
+    }
+}
